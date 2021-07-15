e0345377b292fa2bc993b34fd1c7288119bb537e
Ankur Phani <contact.ankurphani@gmail.com>
2021-07-15 23:08:41 +0530

    Table Component Added

--- src/App.js
+++ src/App.js
@@ -4,42 +4,13 @@ import MaterialTable from "material-table";
 import Pie from './Components/Pie'
 import Charts from './Components/Charts'
 import BarChart from "./Components/Bar";
+import Table from "./Components/Table";
 
 export default function App() {
-  const [data, setData] = useState([]);
-
-
-  const columns = [
-    //{ title: "ID", field: "id" },
-    //{ title: "Username", field: "username" },
-    { title: "Name", field: "name" },
-    { title: "Job Role", field: "jobRole" },
-    { title: "Level", field: "proficiencyLevel" },
-    { title: "State", field: 'state' },
-    { title: "City", field: 'city' },
-    { title: "LinkedIn", field: 'linkedinUrl' }
-  ]
-
-
-  const getData = () => {
-    axios.post('https://nwmxjrs4cb.execute-api.ap-south-1.amazonaws.com/prod/talent/profiles')
-    //.then(res=> console.log(JSON.stringify(res.data.profiles)))
-    .then(res=> setData(res.data))
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
-console.log(data.profiles)
+  
   return (
     <>
-    <div>
-      <MaterialTable
-        title="Employee Data"
-        data={data.profiles}
-        columns={columns}
-      />
-    </div> 
+    <Table/>
     <Charts/>
     <Pie/>
     <BarChart/>
---
+++ src/Components/Table.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react'
+import MaterialTable from "material-table";
+import axios from 'axios'
+
+function Table() {
+    const [data, setData] = useState([]);
+
+  const columns = [
+    //{ title: "ID", field: "id" },
+    //{ title: "Username", field: "username" },
+    { title: "Name", field: "name" },
+    { title: "Job Role", field: "jobRole" },
+    { title: "Level", field: "proficiencyLevel" },
+    { title: "State", field: 'state' },
+    { title: "City", field: 'city' },
+    { title: "LinkedIn", field: 'linkedinUrl' }
+  ]
+
+
+  const getData = () => {
+    axios.post('https://nwmxjrs4cb.execute-api.ap-south-1.amazonaws.com/prod/talent/profiles')
+    //.then(res=> console.log(JSON.stringify(res.data.profiles)))
+    .then(res=> setData(res.data))
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+    return (
+            <MaterialTable
+        title="Employee Data"
+        data={data.profiles}
+        columns={columns}
+      />
+    )
+}
+
+export default Table
